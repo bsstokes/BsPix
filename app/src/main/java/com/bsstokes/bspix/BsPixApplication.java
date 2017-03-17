87e9851fb46eb2f01f3f1c3d37addeeec6bffd76
Brian Stokes <bsstokes@gmail.com>
2017-03-17 15:38:53 -0500

    Integrate LeakCanary.

--- app/src/main/java/com/bsstokes/bspix/BsPixApplication.java
+++ app/src/main/java/com/bsstokes/bspix/BsPixApplication.java
@@ -3,12 +3,19 @@ package com.bsstokes.bspix;
 import android.app.Application;
 
 import com.bsstokes.bspix.initializers.StethoInitializer;
+import com.squareup.leakcanary.LeakCanary;
 
 public class BsPixApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            return;
+        }
+        LeakCanary.install(this);
+
         StethoInitializer.initialize(this);
     }
 }
